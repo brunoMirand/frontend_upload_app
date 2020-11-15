3ef90364a5d958ce7b949db1a01fbccf428cbb91
bruno miranda <bruno.silva>
2020-11-14 21:36:54 -0300

    feat(FRONTEND): [BR] home to upload files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+import { Container, Content } from './styles';
+
+import Upload from './components/Upload';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Content>
+        <Upload />
+      </Content>
+      <GlobalStyles />
+    </Container>
+  )
 }
 
 export default App;
